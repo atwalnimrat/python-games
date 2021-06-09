351164464d388312c8a23e800034720f5c43e79e
Nimrat <atwalnimrat5@gmail.com>
2021-06-09 11:35:17 +0530

    #1 feat: close window

--- space-invader/main.py
+++ space-invader/main.py
@@ -1,1 +1,13 @@
 import pygame
+pygame.init()
+
+# Creating the screen
+screen = pygame.display.set_mode((800,600))
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        # For closing the window
+        if event.type == pygame.QUIT:
+            running = False
