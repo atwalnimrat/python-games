aa50855e0a32f3d6a084a1c2ee5ea88257219594
Nimrat <atwalnimrat5@gmail.com>
2021-06-17 13:17:07 +0530

    #2 feat: add movement

--- space-invader/main.py
+++ space-invader/main.py
@@ -11,9 +11,10 @@ pygame.display.set_icon(icon)
 
 # Player ship
 player_ship = pygame.image.load('space-invader\\player.png')
-player_x,player_y = 365,500
-def player():
-    screen.blit(player_ship,(player_x,player_y))
+player_x,player_y = 365,520
+player_x_change = 0
+def player(x,y):
+    screen.blit(player_ship,(x,y))
 
 # Game loop
 running = True
@@ -22,11 +23,22 @@ while running:
     # Setting background color
     screen.fill((0,0,0))
     
-    # For closing the window
     for event in pygame.event.get():
+        
+        # For closing the window
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+    # For moving the player ship
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                player_x_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_LEFT,pygame.K_RIGHT):
+                player_x_change = 0
+    player_x += player_x_change
+    player(player_x,player_y)
 
     pygame.display.update()
