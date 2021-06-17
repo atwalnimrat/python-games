68964d372f92da09886d854acfeddce61167eac8
Nimrat <atwalnimrat5@gmail.com>
2021-06-17 20:10:43 +0530

    #2 feat: add boundaries & intro

--- space-invader/main.py
+++ space-invader/main.py
@@ -11,20 +11,59 @@ pygame.display.set_icon(icon)
 
 # Player ship
 player_ship = pygame.image.load('space-invader\\player.png')
-player_x,player_y = 365,520
+player_x,player_y = 370,600
 player_x_change = 0
 def player(x,y):
     screen.blit(player_ship,(x,y))
+
+# Intro
+def intro(x,y):
+    screen.fill((41,44,75))
+    intro_run = True
+    while intro_run:
+        if y > 100:
+            y -= 0.2
+            player(x,y)
+        else:
+            intro_run = False
+        pygame.display.update()
+    return y
+def game_banner(x,y):
+    intro_img = pygame.image.load('space-invader\\intro.png')
+    text_img = pygame.image.load('space-invader\\text.png')
+    screen.blit(intro_img,(x,y))
+    screen.blit(text_img,(x-100,y+150))
+    pygame.display.update()
+    show = True
+    while show:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                show = False
+    return
+def intro2(x,y):
+    screen.fill((41,44,75))
+    intro_run = True
+    while intro_run:
+        if y < 520:
+            y += 0.1
+            player(x,y)
+        else:
+            intro_run = False
+        pygame.display.update()
+    return y
+player_y = intro(player_x,player_y)
+game_banner(190,220)
+player_y = intro2(player_x,player_y)
 
 # Game loop
 running = True
 while running:
 
     # Setting background color
-    screen.fill((0,0,0))
+    screen.fill((41,44,75))
     
     for event in pygame.event.get():
-        
+                
         # For closing the window
         if event.type == pygame.QUIT:
             running = False
@@ -39,6 +78,13 @@ while running:
             if event.key in (pygame.K_LEFT,pygame.K_RIGHT):
                 player_x_change = 0
     player_x += player_x_change
-    player(player_x,player_y)
 
+    # Creating boundaries
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= 736:
+        player_x = 736
+
+    player(player_x,player_y)
+    
     pygame.display.update()
