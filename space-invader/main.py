5c5d395e9c3f45bff43722c24466abc664cf103c
Nimrat <atwalnimrat5@gmail.com>
2021-06-17 12:31:41 +0530

    #2 feat: create protagonist

--- space-invader/main.py
+++ space-invader/main.py
@@ -5,20 +5,28 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 # Styling the window
-pygame.display.set_caption("Space Invader")
+pygame.display.set_caption('Space Invader')
 icon = pygame.image.load('space-invader\\ship.png')
 pygame.display.set_icon(icon)
+
+# Player ship
+player_ship = pygame.image.load('space-invader\\player.png')
+player_x,player_y = 365,500
+def player():
+    screen.blit(player_ship,(player_x,player_y))
 
 # Game loop
 running = True
 while running:
-    for event in pygame.event.get():
+
+    # Setting background color
+    screen.fill((0,0,0))
     
-        # For closing the window
+    # For closing the window
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    # Setting background color
-    screen.fill((0,0,50))
+
+    player()
 
     pygame.display.update()
