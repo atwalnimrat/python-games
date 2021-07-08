97e3c3102e72b78ed523e0aedf3a7978d4d64122
Nimrat <atwalnimrat5@gmail.com>
2021-07-08 19:34:31 +0530

    #11 feat: add snake movement

--- snake/main.py
+++ snake/main.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 pygame.init()
 
 # Creating the window
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((450,450))
 
 # Styling the window
 pygame.display.set_caption('Snake')
@@ -13,11 +13,13 @@ pygame.display.set_icon(icon)
 
 # Game variables
 cell_size = 10
+direction = 2        # 1 is up, 2 is right, 3 is down and 4 is left
+update_snake = 0
 
 # Snake
-snake_pos = [[250,250]]
+snake_pos = [[0,225]] #235
 for i in range(1,4):
-    snake_pos.append([250-(i*cell_size),250])
+    snake_pos.append([snake_pos[0][0]-(i*cell_size),snake_pos[0][1]])
 
 # Game loop
 running = True
@@ -26,10 +28,37 @@ while running:
     # Setting background color
     screen.fill((255,200,150))
 
-    # For closing the window
     for event in pygame.event.get():
+        
+        # For closing the window
         if event.type == pygame.QUIT:
             running = False
+
+        # For snake movement 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != 3:
+                direction = 1
+            if event.key == pygame.K_DOWN and direction != 1:
+                direction = 3
+            if event.key == pygame.K_LEFT and direction != 2:
+                direction = 4
+            if event.key == pygame.K_RIGHT and direction != 4:
+                direction = 2
+    if update_snake > 129: #499
+        update_snake = 0
+        snake_pos = snake_pos[-1:] + snake_pos[:-1]
+        if direction == 1:
+            snake_pos[0][0] = snake_pos[1][0]
+            snake_pos[0][1] = snake_pos[1][1] - cell_size
+        if direction == 3:
+            snake_pos[0][0] = snake_pos[1][0]
+            snake_pos[0][1] = snake_pos[1][1] + cell_size
+        if direction == 4:
+            snake_pos[0][0] = snake_pos[1][0] - cell_size
+            snake_pos[0][1] = snake_pos[1][1]
+        if direction == 2:
+            snake_pos[0][0] = snake_pos[1][0] + cell_size
+            snake_pos[0][1] = snake_pos[1][1]
 
     # Snake
     head = 1
@@ -42,5 +71,7 @@ while running:
             pygame.draw.rect(screen,(50,175,25),(j[0]+1,j[1]+1,cell_size-2,cell_size-2))
 
     pygame.display.update()
+
+    update_snake += 1
 
 pygame.quit()
